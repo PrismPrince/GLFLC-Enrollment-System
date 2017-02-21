a1ab630a25552a061ab3a5441f5686aedf2ef1ae
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-02-22 02:59:52 +0800

    Add links on navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,9 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{ route('student.index') }}">Students</a></li>
+                        <li><a href="{{ route('level.index') }}">Grade Levels</a></li>
+                        <li><a href="{{ route('schoolyear.index') }}">School Years</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
